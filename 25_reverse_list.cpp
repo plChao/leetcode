40c186e6f1c5de21d15c7a95736f6615a4b065a0
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2022-02-25 15:35:44 +0800

    [25] a accepted version but slow.

--- 25_reverse_list.cpp
+++ 25_reverse_list.cpp
@@ -31,58 +31,26 @@ void print_list(ListNode *head){
 
 class Solution {
 public:
-    void reverselist(ListNode* head, ListNode* tail, int len){
-        ListNode * cur, *left_head, *left_tail, *right_head, *right_tail, *center;
-        if(len == 1){
-            return;
+    ListNode* reverselist(ListNode* head){
+        ListNode *newhead = nullptr, *next_cur;
+        ListNode *cur = head;
+        while(cur != nullptr){
+            next_cur = cur->next;
+
+            cur->next = newhead;
+            newhead = cur;
+            
+            cur = next_cur;
         }
-        if(len % 2 == 1){
-            int mid = len / 2 - 1;
-            cur = head;
-            for(int i=0;i<mid;i++){
-                cur = cur->next;
-            }
-            left_tail = cur;
-            center = left_tail->next;
-            right_head = center->next;
-            left_head = head;
-            right_tail = tail;
-            reverselist(left_head, left_tail, len/2);
-            reverselist(right_head, right_tail, len/2);
-            // reverse
-            // link right->center->left
-            head = right_head;
-            right_tail->next = center;
-            center->next = left_head;
-            tail = left_tail;
-            left_tail->next = nullptr;
-            return;
-        }
-        else{
-            int mid = len / 2 - 1;
-            cur = head;
-            for(int i=0;i<mid;i++){
-                cur = cur->next;
-            }
-            left_head = head;
-            left_tail = cur;
-            right_head = left_tail->next;
-            right_tail = tail;
-            reverselist(left_head, left_tail, len/2);
-            reverselist(right_head, right_tail, len/2);
-            // link right->left
-            head = right_head;
-            right_tail->next = left_head;
-            tail = left_tail;
-            return;
-        }
+        return newhead;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
         int pos = 1;
-        ListNode *tar_head, *tar_tail; 
+        ListNode *tar_head, *tar_tail, *next_cur; 
         ListNode *pre_head = nullptr, *pos_tail;
         ListNode *cur = head;
         while(cur != nullptr){
+            next_cur = cur->next;
             if(pos == 1){
                 tar_head = cur;
             }
@@ -90,10 +58,10 @@ public:
                 tar_tail = cur;
                 pos_tail = tar_tail->next;
                 tar_tail->next = nullptr;
-                print_list(tar_head);
-                reverselist(tar_head, tar_tail, k);
-                tar_tail->next = nullptr;
-                print_list(tar_head);
+                // print_list(tar_head);
+                tar_tail = tar_head;
+                tar_head  = reverselist(tar_head);
+                // print_list(tar_head);
                 if(pre_head == nullptr){
                     head = tar_head;
                 }
@@ -102,9 +70,10 @@ public:
                 }
                 tar_tail->next = pos_tail;
                 pos = 0;
-                pre_head = cur;
+                pre_head = tar_tail;
             }
-            cur = cur->next;
+            // cout << "cur" << cur->val << endl;
+            cur = next_cur;
             pos += 1;
         }
         return head;
@@ -116,11 +85,12 @@ int main(int argc, char const *argv[])
     ListNode* head = nullptr;
     ListNode* cur;
     for(int i=0;i<5;i++){
-        cur = new ListNode(i, head);
+        cur = new ListNode(i+1, head);
         head = cur;
     }
-    print_list(head);
     Solution s = Solution();
+    head = s.reverselist(head);
+    print_list(head);
     head = s.reverseKGroup(head, k);
     cout << "out" << endl;
     print_list(head);

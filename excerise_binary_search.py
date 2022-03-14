4904daab0a3c54cfd8c6a4833b4d0fe8da696869
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2022-03-14 22:59:59 +0800

    [excerise binary search][assert pass] change dsc similar to asc.

--- excerise_binary_search.py
+++ excerise_binary_search.py
@@ -41,10 +41,10 @@ def upper_bound_dsc(nums, target):
     # it means nums[l] >= target >= nums[r]
     while l <= r:
         mid = (l+r) // 2
-        if nums[mid] > target:
-            l = mid + 1
-        else:
+        if nums[mid] <= target:
             r = mid - 1
+        else:
+            l = mid + 1
     return l - 1
 
 def lower_bound_dsc(nums, target):
@@ -57,11 +57,10 @@ def lower_bound_dsc(nums, target):
     # it means nums[l] >= target >= nums[r]
     while l <= r:
         mid = (l+r) // 2
-        print(l, r, mid)
-        if nums[mid] >= target:
-            l = mid + 1
-        else:
+        if nums[mid] < target:
             r = mid - 1
+        else:
+            l = mid + 1
     return l - 1
 
 if __name__ == '__main__':

20da608058d1fe210f523690e73bc9c22f59c15f
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2022-04-23 11:19:08 +0800

    [1][Accepted][hash_map-2] a soultion using one loop. Using unordered map to implement hash map.

--- 1_twoSum.cpp
+++ 1_twoSum.cpp
@@ -4,10 +4,8 @@
 #include<algorithm>
 #include<unordered_map>
 #include<cmath>
-#include<hash_map> // need to paste to leetcode
 
 using namespace std;
-using namespace __gnu_cxx; // need to paste to leetcode
 
 void print_int_vec(vector<int> tar){
     for(auto i: tar){
@@ -19,18 +17,16 @@ void print_int_vec(vector<int> tar){
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        hash_map<int, int> mymap;
+        unordered_map<int, int> mymap;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            mymap[nums[i]] = i;
-        }
-        for(int i=0;i<nums.size();i++){
             int remain = target - nums[i];
-            if(mymap.count(remain) && mymap[remain] != i){
-                ans.push_back(i);
+            if(mymap.find(remain) != mymap.end()){
                 ans.push_back(mymap[remain]);
+                ans.push_back(i);
                 return ans;
             }
+            mymap[nums[i]] = i;
         }
         return ans;
     }

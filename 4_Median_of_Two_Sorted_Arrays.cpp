623df879ffb6eae77b1adfb2f1c34ba71c54a6f6
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2022-10-20 19:26:52 +0800

    [4][AC] using binary search for the right position to cut two arry, tO(log min(m, n)), sO(1). With only one side of judge.

--- 4_Median_of_Two_Sorted_Arrays.cpp
+++ 4_Median_of_Two_Sorted_Arrays.cpp
@@ -7,7 +7,8 @@
 #include<algorithm>
 #include<cctype>
 #include<set>
-#include<map> 
+#include<map>
+#include<assert.h> 
 using namespace std;
 
 void print_int_vec(vector<int> tar){
@@ -19,61 +20,76 @@ void print_int_vec(vector<int> tar){
 
 class Solution {
 public:
-    pair<int, int> findithelement(vector<int>& nums1, vector<int>& nums2, int i){
-        nums1.push_back(INT_MAX);
-        nums2.push_back(INT_MAX);
-        auto ptr1 = nums1.begin();
-        auto ptr2 = nums2.begin();
-        int before_ith_element = *ptr1;
-        cout << "i " << i << endl;
-        int cnt = 0;
-        while(cnt < i){
-            if(*ptr1 < *ptr2){
-                cnt++;
-                if(cnt == i){
-                    before_ith_element = *ptr1;
-                }
-                ptr1++;
-            }
-            else{
-                cnt++;
-                if(cnt == i){
-                    before_ith_element = *ptr2; 
-                }
-                ptr2++;
-            }
-        }
-        if(*ptr1 < *ptr2){
-            // nums1.pop_back();
-            // nums2.pop_back();
-            return make_pair(before_ith_element, *ptr1);
+    // cutsize: (n1 + n2) // 2 = 5
+    // nums1 : 1, 2, 3 || 4, 5 ,6
+    //                    ^ cutfor1 (3)
+    // nums1 : 1, 2 || 3, 4, 5
+    //                 ^ cutfor2 (2)
+
+    bool cutright(vector<int>& nums1, vector<int>& nums2, int cutfor1, int cutsize){
+        int cutfor2 = cutsize - cutfor1;
+        int n1 = nums1.size(), n2 = nums2.size();
+        int l1, l2, r1, r2;
+        l1 = (cutfor1 == 0? INT_MIN: nums1[cutfor1-1]);
+        l2 = (cutfor2 == 0? INT_MIN: nums2[cutfor2-1]);
+        r1 = (cutfor1 >= n1? INT_MAX: nums1[cutfor1]);
+        r2 = (cutfor2 >= n2? INT_MAX: nums2[cutfor2]); 
+        if(l2 < r1){
+            return true;
         }
         else{
-            // nums1.pop_back();
-            // nums2.pop_back();
-            return make_pair(before_ith_element, *ptr2); 
+            return false;
         }
-
     }
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() < nums2.size()){
+        int n1 = nums1.size(), n2 = nums2.size();
+        int cutsize = (n1 + n2) / 2;
+
+        if(n1 > n2){
             return findMedianSortedArrays(nums2, nums1);
         }
-        if(nums2.size() == 0){
-            int length = nums1.size();
-            return (nums1[int((length - 1) / 2)] + nums1[length/2]) / 2.0;
+        // n1 < n2
+        if(n1 == 0){
+            return (nums2[int((n2 - 1) / 2)] + nums2[n2 / 2]) / 2.0;
         }
-        pair<int, int> middle_element;
-        int sizesum = nums1.size() + nums2.size();
-        if(sizesum % 2 == 0){
-            middle_element = findithelement(nums1, nums2, sizesum / 2);
-            // cout << "mid " << middle_element.first << " " << middle_element.second << endl;
-            return (middle_element.first + middle_element.second) / 2.0;
+
+        for(int i=0;i<n1;i++){
+            cout << cutright(nums1, nums2, i, cutsize) << " ";
+        }
+        cout << endl;
+        
+        int l = 0, r = n2 - 1;
+        while (l <= r)
+        {
+            int mid = (l+r) / 2;
+            if(cutright(nums1, nums2, mid, cutsize)){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+
+        int cutfor1 = l;
+        int cutfor2 = cutsize - cutfor1;
+        int l1, l2, r1, r2;
+        
+        l1 = (cutfor1 == 0? INT_MIN: nums1[cutfor1-1]);
+        l2 = (cutfor2 == 0? INT_MIN: nums2[cutfor2-1]);
+        r1 = (cutfor1 >= n1? INT_MAX: nums1[cutfor1]);
+        r2 = (cutfor2 >= n2? INT_MAX: nums2[cutfor2]); 
+
+        // if(l1 < r2 && l2 < r1){
+        //     cout << "pass" << endl;
+        // }
+        // else{
+        //     cout << l << endl;
+        // }
+        if((n1+n2) % 2 == 0){
+            return (max(l1, l2) + min(r1, r2)) / 2.0;
         }
         else{
-            middle_element = findithelement(nums1, nums2, (sizesum + 1) / 2);
-            // cout << "mid " << middle_element.first << endl;
-            return middle_element.first;
+            return min(r1, r2);
         }
     }
 };
@@ -81,7 +97,7 @@ int main(int argc, char const *argv[])
 {   
     Solution solve;
     vector<int> inputa{1, 3, 4, 5};
-    vector<int> inputb{2};
+    vector<int> inputb{2, 4};
     cout << solve.findMedianSortedArrays(inputa, inputb) << endl;
     return 0;
 }

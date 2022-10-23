3522580669714c8e74848f10edae872bfe68cb52
plchao <poseidon31418926.cs06@g2.nctu.edu.tw>
2022-10-23 15:05:46 +0800

    [16][AC] tO(n^2) sO(n), skipping first level seen, two ptr approach.

--- 16_3Sum_Closest.cpp
+++ 16_3Sum_Closest.cpp
@@ -20,33 +20,28 @@ void print_int_vec(vector<int> tar){
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        // unordered_set<int> first_seen;
-        // unordered_set<int> combine_seen;
+        unordered_set<int> first_seen;
         sort(nums.begin(), nums.end());
         int closest_diff = nums[0] + nums[1] + nums[2] - target;
         // cout << closest_diff << endl;
         for(int i=0;i<nums.size()-2;i++){
-            // if(!first_seen.insert(nums[i]).second){
-            //     // cout << "first seen skip" << endl;
-            //     continue;
-            // }
-            for(int j=i+1;j<nums.size()-1;j++){
-                int combine = nums[i] + nums[j];
-                // if(!combine_seen.insert(nums[i] + nums[j]).second){
-                //     continue;
-                // }
-                int remain = target - combine;
-                int upper_closest_idx = upper_bound(nums.begin()+j+1, nums.end(), remain) - nums.begin();
-                int lower_closest_idx = upper_closest_idx - 1;
-                if(upper_closest_idx < nums.size()){
-                    int up_diff = (combine + nums[upper_closest_idx]) - target;
-                    // cout << up_diff << " " << closest_diff << endl;
-                    closest_diff = (abs(up_diff) < abs(closest_diff))?up_diff:closest_diff;
+            if(!first_seen.insert(nums[i]).second){
+                continue;
+            }
+            int remain = target - nums[i];
+            int lo = i+1, hi = nums.size() - 1;
+            while(lo < hi){
+                if(abs(nums[lo] + nums[hi] - remain) < abs(closest_diff)){
+                    closest_diff = nums[lo] + nums[hi] - remain;
                 }
-                if(lower_closest_idx > j){
-                    int lo_diff = (combine + nums[lower_closest_idx]) - target;
-                    // cout << lo_diff << " " << closest_diff << endl;
-                    closest_diff = (abs(lo_diff) < abs(closest_diff))?lo_diff:closest_diff;
+                if(nums[lo] + nums[hi] == remain){
+                    return target;
+                }
+                else if(nums[lo] + nums[hi] < remain){
+                    lo ++;
+                }
+                else{
+                    hi --;
                 }
             }
         }
